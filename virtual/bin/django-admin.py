ee38f8c2391ff5e6644df51b37381384317ea158
Dan Njoroge <greatdaniels@gmail.com>
2020-06-05 17:11:21 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/dannyboy/Documents/python/django/projects/awwards-app/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
