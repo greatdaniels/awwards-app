a27c27eb54243c5e6e62d9b4a90e51969ee5785e
Dan Njoroge <greatdaniels@gmail.com>
2020-06-07 16:52:18 +0300

    adding User form

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -17,3 +17,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('photo', 'title', 'url', 'description',)
+
+class UserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta: 
+        model = User
+        fields = ('username', 'email')
