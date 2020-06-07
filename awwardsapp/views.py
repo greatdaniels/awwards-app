ebe0123e3d25d2c8742bb4e401a2b15ff6f0e9c9
Dan Njoroge <greatdaniels@gmail.com>
2020-06-07 17:10:31 +0300

    adding index to views.py

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,5 +1,28 @@
+import random
 from django.shortcuts import render
+from .forms import PostForm, SignupForm, UserForm, ProfileForm, RatingsForm
+from .models import Post, Profile, Rating
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post.photo)
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
