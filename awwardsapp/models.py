72397124ddeadf239c46c75c83354c281170769f
Dan Njoroge <greatdaniels@gmail.com>
2020-06-07 16:05:51 +0300

    adding class Post for projects

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -11,8 +11,6 @@ class Profile(models.Model):
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
-    location = models.CharField(max_length=60, blank=True)
-    contact = models.EmailField(max_length=100, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -25,3 +23,29 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    photo = ImageField(manual_crop='1280x720')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
+
